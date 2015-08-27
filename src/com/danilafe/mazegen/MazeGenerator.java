014887acdb9eaa836784bef948112a8ec1362b88
DanilaFe <danila.fedorin@gmail.com>
2015-08-26 17:30:57 -0700

    Possibly fix a stack overflow in the recursive function for backtracking, flood fill up next

--- src/com/danilafe/mazegen/MazeGenerator.java
+++ src/com/danilafe/mazegen/MazeGenerator.java
@@ -69,7 +69,7 @@ public class MazeGenerator {
 					};
 			} while (getArrayValue(mazeArray, cursorPos[0], cursorPos[1]) != 1);
 			
-			backtrack(mazeArray, cursorPos[0], cursorPos[1], cursorPos[0], cursorPos[1], width, height);
+			backtrack(mazeArray, cursorPos[0], cursorPos[1], cursorPos[0], cursorPos[1], width, height, 0, 256, cursorPos);
 		} while(containsValue(mazeArray, (byte) 1));
 	}
 	
@@ -90,7 +90,7 @@ public class MazeGenerator {
 	 * @param mazeWidth the width of the maze being generated.
 	 * @param mazeHeight the height of the maze being generated.
 	 */
-	private static void backtrack(final byte[][] array, final int pos_x, final int pos_y, final int pos_prevx, final int pos_prevy, final int mazeWidth, final int mazeHeight){
+	private static boolean backtrack(final byte[][] array, final int pos_x, final int pos_y, final int pos_prevx, final int pos_prevy, final int mazeWidth, final int mazeHeight, final int currentSize, final int maxSize, final int[] lastCell){
 		writeBetween(array, pos_x, pos_y, pos_prevx, pos_prevy, (byte) 2); 
 		writeToMazeRarray(array, pos_x, pos_y, (byte) 2);
 		while(!isSurrounded(array, pos_x, pos_y)){
@@ -116,20 +116,33 @@ public class MazeGenerator {
 			}
 			if((newCheckPos[0] < 0) || (newCheckPos[0] > mazeWidth - 1) || (newCheckPos[1] < 0) || (newCheckPos[1] > mazeHeight - 1)) continue;
 			if(getArrayValue(array, newCheckPos[0], newCheckPos[1]) != 2 && getArrayValue(array, newCheckPos[0], newCheckPos[1]) != 3){
+				if(currentSize == maxSize) {
+					lastCell[0] = newCheckPos[0];
+					lastCell[1] = newCheckPos[1];
+					return true;
+				}
 				if(threadCount < MAX_THREADS){
 					threadCount += 1;
 					Thread backtrackThread = new Thread("Backtracking Thread " + threadCount){
 						public void run(){
-							backtrack(array, newCheckPos[0], newCheckPos[1], pos_x, pos_y, mazeWidth, mazeHeight);
+							int[] newLastCell = new int[]{
+									newCheckPos[0],
+									newCheckPos[1]
+							};
+							while(backtrack(array, newLastCell[0], newLastCell[1], pos_x, pos_y, mazeWidth, mazeHeight, 0 , maxSize, lastCell));
 							threadCount -= 1;
 						}
 					};
 					backtrackThread.start();
 				}
-				backtrack(array, newCheckPos[0], newCheckPos[1], pos_x, pos_y, mazeWidth, mazeHeight);
+				if(backtrack(array, newCheckPos[0], newCheckPos[1], pos_x, pos_y, mazeWidth, mazeHeight, currentSize + 1, maxSize, lastCell)){
+					return true;
+				}
 			}
 			
 		}
+		
+		return false;
 	}
 	
 	/**
